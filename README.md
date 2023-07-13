9501e01df07202f83c89afeb867615d7c2db7d4f
hesam <hesam@LAPTOP-K4EHTU8F>
2023-07-13 08:09:46 -0700

    redesigned the UI, add scoreboard for two players

---
+++ README.md
@@ -0,0 +1,19 @@
+# JeoParty
+
+## Description
+
+JeoParty is a trivia game for two teams. The app randomly selects 6 categories and 5 clues per category, and players can click on the cells in the Jeoparty table to reveal the questions or answers. The team with the highest score at the end of the game wins.
+
+## Features
+
+- Randomly generated questions and answers
+- Two-player mode
+- Scoreboard to track the progress of each team
+- Clickable cells to reveal the questions or answers
+
+## Technologies Used
+The JeoParty app is built using the following technologies:
+- JavaScript
+- jQuery
+- HTML
+- CSS
--- jeopardy.js
+++ jeopardy.js
@@ -5,159 +5,151 @@ const NUM_CAT = 6;
 const NUM_Q = 5;
 
 
-// categories is the main data structure for the app; it looks like this:
-
-//  [
-//    { title: "Math",
-//      clues: [
-//        {question: "2+2", answer: 4, showing: null},
-//        {question: "1+1", answer: 2, showing: null}
-//        ...
-//      ],
-//    },
-//    { title: "Literature",
-//      clues: [
-//        {question: "Hamlet Author", answer: "Shakespeare", showing: null},
-//        {question: "Bell Jar Author", answer: "Plath", showing: null},
-//        ...
-//      ],
-//    },
-//    ...
-//  ]
-
+let scoreTeamA = 0;
+let scoreTeamB = 0;
+let lastClickedNumTeamA = 0; 
+let lastClickedNumTeamB = 0; 
 let categories = [];
 
-/** Get NUM_CATEGORIES random category from API.
- *
- * Returns array of category ids
- */
 
+
+// Gets a list of category IDs from the Jeopardy API and returns a random sample of NUM_CAT IDs.
 async function getCategoryIds() {
     let response = await axios.get(`${BASE_URL}/categories?count=100`)
     let catId = [];
-        // console.log(response.data);
+
     for (let el of response.data) {
         catId.push(el.id)
     }
-        // console.log(catId)
-        // console.log(_.sampleSize(catId, NUM_CAT))
         return (_.sampleSize(catId, NUM_CAT))
 }
 
 
-/** Return object with data about a category:
- *
- *  Returns { title: "Math", clues: clue-array }
- *
- * Where clue-array is:
- *   [
- *      {question: "Hamlet Author", answer: "Shakespeare", showing: null},
- *      {question: "Bell Jar Author", answer: "Plath", showing: null},
- *      ...
- *   ]
- */
 
+// Gets a category from the Jeopardy API, returns a randomly selected set of clues from the category.
 async function getCategory(catId) {
     
     let result = await axios.get(`${BASE_URL}/category?id=${catId}`)
-        // console.log(result.data)
     let category = result.data
     let catTitle = category.title
-        // console.log(catTitle)
     const catTitleUP = catTitle.toUpperCase()
-        // console.log(typeof(catTitle))
     let catClues = category.clues
     let randomClues = _.sampleSize((catClues), NUM_CAT)
-        // console.log(catClues)
     let clues = randomClues.map( el => ({question: el.question, answer: el.answer, airdate: el.airdate, showing: null}));
-       
-        // console.log({ title: catTitleUP, clues})
         return { title: catTitleUP, clues}
 }
 
 
-/** Fill the HTML table#jeopardy with the categories & cells for questions.
- *
- * - The <thead> should be filled w/a <tr>, and a <td> for each category
- * - The <tbody> should be filled w/NUM_QUESTIONS_PER_CAT <tr>s,
- *   each with a question for each category in a <td>
- *   (initally, just show a "?" where the question/answer would go.)
- */
 
+// Fills the Jeopardy table with the category titles and randomly generated numbers, and adds a click event listener to each cell.
 async function fillTable() {
-    // fill the table header using the selected categories 
     $("#jeopardy thead").empty();
     let $tr = $('<tr>');
     for (let i = 0; i < NUM_CAT; i++) {
-        // $tr.append($('<th>').text(categories[i].title))
         $tr.append($('<th>').text(categories[i].title))
     }
-            $('#jeopardy thead').append($tr)
+    $('#jeopardy thead').append($tr)
     
-    // fill rows with questions  
+    let randomNumbers = [100, 200, 400, 600, 800, 1000];
     $("#jeopardy tbody").empty(); 
     for (let j = 0; j < NUM_Q; j++ ) {
+        
         let $tr = $('<tr>');
             for (let i = 0; i < NUM_CAT; i++) {
-                $tr.append($('<td>').attr("id", `${i}-${j}`).text("?"));
+                randomNumbers.sort(() => Math.random() - 0.5);
+                let randomNum = randomNumbers[i];
+                $tr.append($('<td>').attr("id", `${i}-${j}`).text(randomNum));
             }
             $('#jeopardy thead').append($tr)
     }
 }
 
-/** Handle clicking on a clue: show the question or answer.
- *
- * Uses .showing property on clue to determine what to show:
- * - if currently null, show question & set .showing to "question"
- * - if currently "question", show answer & set .showing to "answer"
- * - if currently "answer", ignore click
- * */
 
+
+// Handles a click on a Jeopardy table cell and displays the question or answer for the clue, depending on whether the clue has been revealed yet.
 function handleClick(evt) {
-    let id = evt.target.id
-    let [catId, clueId] = id.split("-")
-    let clue = categories[catId].clues[clueId]
-    // console.log(clue.showing)
-    let airdate = (clue.airdate).slice(0,10)
-    if (clue.showing === null ) {
-        $(`#${catId}-${clueId}`).html(clue.question + " - (Question's airdate : " + airdate + ")" );
-        $(`#${catId}-${clueId}`).css("background-color", "#74119c") 
-        clue.showing = "question"
-    } else if (clue.showing === "question") {
+    let id = evt.target.id;
+    let [catId, clueId] = id.split("-");
+    let clue = categories[catId].clues[clueId];
+    let randomNum = parseInt($(evt.target).text());
+    
+    lastClickedNumTeamA = randomNum ;
+    lastClickedNumTeamB = randomNum;
+    
+    if (clue === undefined) {
+        $(`#${catId}-${clueId}`).html(
+            "No information"
+        );
+        $(`#${catId}-${clueId}`).css("background-color", "#ecf230"); 
+    }
+
+    else {                    
+        let airdate = null;
+        if (clue && clue.airdate !== undefined && clue.airdate !== null) {
+            airdate = clue.airdate.slice(0, 10);
+        }
+    
+
+        if (clue && clue.showing === null) {
+        $(`#${catId}-${clueId}`).html(
+            clue.question + " - (Question's airdate : " + airdate + ")"
+        );
+        $(`#${catId}-${clueId}`).css("background-color", "#74119c");
+        clue.showing = "question";
+        } else if (clue && clue.showing === "question") {
         $(`#${catId}-${clueId}`).html(clue.answer);
-        $(`#${catId}-${clueId}`).css("background-color", "#28a200")
-        clue.showing = "answer"
-    } else return
+        $(`#${catId}-${clueId}`).css("background-color", "#28a200");
+        clue.showing = "answer";
+        }   
+    }
 
 }
 
-/** Start game:
- *
- * - get random category Ids
- * - get data for each category
- * - create HTML table
- * */
 
+// Updates the scores for the two teams when the corresponding button is clicked.
+const scoreTeamAElement = $("#scoreTeamA");
+const addScoreTeamAButton = $("#addScoreTeamA");
+const subtractScoreTeamAButton = $("#subtractScoreTeamA");
+const scoreTeamBElement = $("#scoreTeamB");
+const addScoreTeamBButton = $("#addScoreTeamB");
+const subtractScoreTeamBButton = $("#subtractScoreTeamB");
+addScoreTeamAButton.on("click", function() {
+scoreTeamA += lastClickedNumTeamA;
+scoreTeamAElement.text(scoreTeamA);
+});
+subtractScoreTeamAButton.on("click", function() {
+scoreTeamA -= lastClickedNumTeamA;
+scoreTeamAElement.text(scoreTeamA);
+});
+addScoreTeamBButton.on("click", function() {
+scoreTeamB += lastClickedNumTeamB;
+scoreTeamBElement.text(scoreTeamB);
+});
+subtractScoreTeamBButton.on("click", function() {
+scoreTeamB -= lastClickedNumTeamB;
+scoreTeamBElement.text(scoreTeamB);
+});
+
+
+
+// Sets up the game by getting the category IDs, creating the categories array, and filling the Jeopardy table.
 async function setupAndStart() {
     console.debug("setupAndStart");
-    
-    let res = await getCategoryIds();
 
+    let res = await getCategoryIds();
     categories = [];
-    
+
     for (let catId of res) {
         categories.push(await getCategory(catId))
     }
 
     fillTable();
-    // console.log("categories =", categories)
 }
 
-/** On click of restart button, restart game. */
 
 $('#btn').on("click", setupAndStart)
 
-/** On page load, setup and start & add event handler for clicking clues */
+
 $(async function () {
     setupAndStart();
     $("#jeopardy").on("click", "td", handleClick)
